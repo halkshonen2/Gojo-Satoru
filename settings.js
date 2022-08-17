64ef2dc482196d61c4281518f0435571039b101c
halkshonen2 <111390352+halkshonen2@users.noreply.github.com>
2022-08-17 18:46:31 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['330667930372']
+global.premium = ['330667930372']
+global.ownernomer = '330667930372'
+global.ownername = 'halkshonen'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
